fd71bff29fa90b6d70b2610210b430a877bd398c
joimar755 <joimarjose19@gmail.com>
2024-03-21 07:20:43 -0500

    a_prsima

--- prisma/migrations/20240321050818_init/migration.sql
+++ prisma/migrations/20240321050818_init/migration.sql
@@ -20,5 +20,12 @@ CREATE TABLE `Category` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Detalle` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Product` ADD CONSTRAINT `Product_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `Category`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
